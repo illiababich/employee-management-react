b8fc049b707e811b11f5a1d7909f39782704125e
illiababich <illia.babich.2@gmail.com>
2023-07-30 23:58:40 +0300

    removing dictionary

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Employees from './pages/navbar/Employees';
 import Header from "./pages/Header";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from "./pages/navbar/Home";
-import Dictionary from "./app2/Dictionary";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Header>
           <Routes>
             <Route path={"/employees"} element={<Employees/>}/>
-            <Route path={"/calendar"} element={<Dictionary/>}/>
             <Route path={"/"} element={<Home/>}/>
           </Routes>
         </Header>
